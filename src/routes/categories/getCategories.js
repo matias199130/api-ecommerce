a7c298d964077af60a52c59892ad910cf44aa702
Brenda Álvarez <bgalacho@gmail.com>
2022-04-22 18:55:57 -0300

    routesUpdated

--- src/routes/categories/getCategories.js
+++ src/routes/categories/getCategories.js
@@ -1,19 +1,29 @@
 
-const { Category } = require('../../db.js');
+const { Category, Product} = require('../../db.js');
+const { Router } = require("express");
+const router = Router();
 
-const getCategories = async (req, res, next) => {
-	try {
+router.get("/", async (req, res) => {
+
 		const allCategories = await Category.findAll();
 
 
         allCategories.length ? res.status(200).send(allCategories) :
 			res.status(401).send('error en el servidor, notifique al administrador');
-	} catch (error) {
-		console.log(error);
-	}
-};
 
+});
 
-module.exports = {
-	getCategories
-};
+router.get("/:id", async (req, res) => {
+	const idCategory  = req.params.id;
+	 const oneCategory = await Category.findOne({
+		where: {
+		  idCategory: idCategory,		  
+		},
+	  });
+	  !idCategory
+		? res.status(404).send("Categoría no encontrada")
+		: res.status(200).send(oneCategory);
+
+  });
+
+  module.exports = router;
